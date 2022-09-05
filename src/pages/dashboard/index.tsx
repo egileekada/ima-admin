ee2ab073039a8db1a15f6c198d3eed363397b82e
Miikor-Jnr <barilelomiikor@gmail.com>
2022-09-05 15:37:26 +0100

    completed the property details

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -6,20 +6,22 @@ import { Property } from "../../components/Property"
 import DashboardListing from "../../components/DashboardListing"
 import Navbar from "../../components/Navbar"
 import DashboardImaOriginal from "../../components/DashboardImaOriginal"
+import { PropertyDetails } from "../../components/propertyDetails"
 
 
 
 const Dashboard: NextPage = () => {
-    const [page, setPage] = useState('dashboard')
+    const [page, setPage] = useState('detail')
 
     return (
         <div>
             <DashboardSideNav page={page} setPage={setPage}>
                 <Navbar />
                 {page==='dashboard' && <DashboardHome />}
-                {page === 'property' && <Property />}
+                {page === 'property' && <Property setPage={setPage}/>}
                 {page === 'listing' && <DashboardListing />}
                 {page === 'original' && <DashboardImaOriginal />}
+                {page === 'detail' && <PropertyDetails />}
             </DashboardSideNav>
         </div>
     )
